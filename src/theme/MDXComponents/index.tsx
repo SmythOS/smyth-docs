06ef114936ab9cc693a03437cd84ac108c95924b
raffyememon <raffye.memon@gmail.com>
2025-06-16 19:09:05 +0500

    typo-fix again

--- src/theme/MDXComponents/index.tsx
+++ src/theme/MDXComponents/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from '@docusaurus/Link';
-import YouTube from '@site/src/components/YouTube';
+import YouTube from '@site/src/components/Youtube';
 import Arcade from '@site/src/components/Arcade';
 import { Card } from "@site/src/components/Card";
 import CardGrid from '@site/src/components/CardGrid';
